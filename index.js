cb1154e228b8d0cac77837091485c6c195741761
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-03-30 21:15:12 -0500

    Added manager questions

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const inquirer = require('inquirer');
 inquirer.prompt([
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: `Please enter the Team Manager's name`,
+        name: 'managerName',
+    },
+    {
+        type: 'input',
+        message: `What is your employee ID?`,
+        name: 'managerID',
+    },
+    {
+        type: 'input',
+        message: `What is your email?`,
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: `What is your office number?`,
+        name: 'managerOffice',
     },
 
 ])
